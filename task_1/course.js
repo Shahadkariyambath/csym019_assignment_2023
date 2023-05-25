afa2ede79ee83eacf1abfc2eadef74aadf56f0ad
shahadkariyambath <shahadchikku@gmail.com>
2023-05-25 21:23:48 +0100

    Added some more course details  in json and also implemented currency conversion function also

--- task_1/course.js
+++ task_1/course.js
@@ -43,18 +43,18 @@
                 module_and_creditsList.appendChild(module_and_creditsItem);
                 });
 
-              datatxt+="<tr><td><a href='"+response.courseDetails[index].link+"'>" +response.courseDetails[index].name+"</a></td><td>"
-              +response.courseDetails[index].Level+"</td><td>"
-              +DurationList.innerHTML+"</td><td>"
-              +StartingList.innerHTML+"</td><td>"
-              +response.courseDetails[index].Fees+"</td><td>"
+              datatxt+="<tr><td><a href='"+response.courseDetails[index].link+"'>" +response.courseDetails[index].name+"</a><br><br><b>Level  </b>"+response.courseDetails[index].Level+"<br><br> <b>Location  </b>  "+response.courseDetails[index].Location+
+              "</td><td >"
+              +DurationList.innerHTML+"</td><td >"
+              +StartingList.innerHTML+"</td><td data-target data-original class='center-align'>"
+              +response.courseDetails[index].Fees+"</td><td data-target data-original class='center-align'>"
               +response.courseDetails[index].Fees_Int+"</td><td>"
-              +response.courseDetails[index].Location+"</td><td>"
               +response.courseDetails[index].course_Overview+"</td><td>"
-              +response.courseDetails[index].english_language_requirements+"</td><td>"
+              +response.courseDetails[index].Feesandfunding+"</td><td class='center-align'>"
+              +response.courseDetails[index].english_language_requirements+"</td><td >"
               +module_and_creditsList.innerHTML+"</td>";
          });
-            $(recipeTable).append(datatxt); 
+            $(courseDetails).append(datatxt); 
             //the data obtained using the loop is stored to txt variable and it is added to the table using the append function and by referring the id for the table defined in tbody.
         },
         error: function(){
@@ -63,4 +63,150 @@
     });
     }, 250);
   }());
+
+
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    // Get references to the buttons
+    var euroButton = document.getElementById("euro");
+    var dollarButton = document.getElementById("dollar");
+    var poundButton = document.getElementById("pound");
+  
+    // Set the initial currency value
+    var currentCurrency = "pound"; // Initial value for Pound
+  
+    // Add event listeners to the buttons
+    euroButton.addEventListener("click", function() {
+      if (currentCurrency === "pound") {
+        convertToCurrency(1.15);
+        currentCurrency = "euro"; // Update the current currency value
+        disableButton(euroButton);
+        enableButton(dollarButton);
+        enableButton(poundButton);
+      } else if (currentCurrency === "dollar") {
+        convertToCurrency(0.93);
+        currentCurrency = "euro"; // Update the current currency value
+        disableButton(euroButton);
+        enableButton(dollarButton);
+        enableButton(poundButton);
+      }
+    });
+  
+    dollarButton.addEventListener("click", function() {
+      if (currentCurrency === "pound") {
+        convertToCurrency(1.23);
+        currentCurrency = "dollar"; // Update the current currency value
+        disableButton(dollarButton);
+        enableButton(euroButton);
+        enableButton(poundButton);
+      } else if (currentCurrency === "euro") {
+        convertToCurrency(1.07);
+        currentCurrency = "dollar"; // Update the current currency value
+        disableButton(dollarButton);
+        enableButton(euroButton);
+        enableButton(poundButton);
+      }
+    });
+  
+    poundButton.addEventListener("click", function() {
+      if (currentCurrency === "euro") {
+        convertToCurrency(0.87);
+        currentCurrency = "pound"; // Update the current currency value
+        disableButton(poundButton);
+        enableButton(euroButton);
+        enableButton(dollarButton);
+      } else if (currentCurrency === "dollar") {
+        convertToCurrency(0.81);
+        currentCurrency = "pound"; // Update the current currency value
+        disableButton(poundButton);
+        enableButton(euroButton);
+        enableButton(dollarButton);
+      }
+    });
+  
+    // Function to convert currency
+    function convertToCurrency(conversionRate) {
+      var targetTDs = document.querySelectorAll("td[data-target]");
+      targetTDs.forEach(function(td) {
+        var tdValue = parseFloat(td.innerText);
+        var convertedValue = tdValue * conversionRate;
+        td.innerText = convertedValue.toFixed(2);
+      });
+    }
+  
+    // Function to disable a button
+    function disableButton(button) {
+      button.disabled = true;
+    }
+  
+    // Function to enable a button
+    function enableButton(button) {
+      button.disabled = false;
+    }
+  
+    // Initially disable the pound button
+    disableButton(poundButton);
+  });
+  
+   
+
+
+  
+//   function disableButton(clickedButtonId) {
+//     var buttons = document.getElementsByClassName("button-container")[0].getElementsByTagName("button");
+
+//     for (var i = 0; i < buttons.length; i++) {
+//       var button = buttons[i];
+      
+//       if (button.id === clickedButtonId) {
+//         button.disabled = true;
+//       } else {
+//         button.disabled = false;
+//       }
+//     }
+//   }
+
+
+//   document.addEventListener("DOMContentLoaded", function() {
+//     var euro = document.getElementById("euro");
+//     euro.addEventListener("click", euro);
+//   });
+
+//   function euro() {
+//     var targetTDs = document.querySelectorAll("td[data-target]");
+//     targetTDs.forEach(function(td) {
+//       var tdValue = parseInt(td.innerText);
+//       var multipliedValue = tdValue * 1.15;
+//       td.innerText = multipliedValue;
+//     });
+//   }
+
+//   document.addEventListener("DOMContentLoaded", function() {
+//     var dollar = document.getElementById("dollar");
+//     dollar.addEventListener("click", dollar);
+//   });
+
+//   function dollar() {
+//     var targetTDs = document.querySelectorAll("td[data-target]");
+//     targetTDs.forEach(function(td) {
+//       var tdValue = parseInt(td.innerText);
+//       var multipliedValue = tdValue * 1.23;
+//       td.innerText = multipliedValue;
+//     });
+//   }
+
+//   document.addEventListener("DOMContentLoaded", function() {
+//     var pound = document.getElementById("pound");
+//     pound.addEventListener("click", pound);
+//   });
+
+//   function pound() {
+//     var targetTDs = document.querySelectorAll("td[data-target]");
+//     targetTDs.forEach(function(td) {
+//       var tdValue = parseInt(td.innerText);
+//       var multipliedValue = tdValue * 1.15;
+//       td.innerText = multipliedValue;
+//     });
+//   }
   
