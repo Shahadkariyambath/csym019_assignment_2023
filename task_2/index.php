d067c6ddabd0f7865ff46f589d09bcd6ba4141c7
shahadkariyambath <shahadchikku@gmail.com>
2023-05-26 23:49:21 +0100

    database for login is setup and design and implemeted login page

---
+++ task_2/index.php
@@ -0,0 +1,93 @@
+<!-- login.php is used for login into the page using username and password data -->
+
+<?php
+include 'dbinstance.php'; // this will include the connect.php file to access the mysql database 
+?>
+
+
+<?php
+
+if (isset($_POST['username'])) {
+
+    $uname = $_POST['username']; //the username is assigned to a variable uname
+    $password = $_POST['password']; //password is assigned to a variable password
+
+    // $result = fetchARecordWithTwoWhereClause('user', 'email', $uname, 'password', $password);
+
+    $stmt = $GLOBALS['pdo']->prepare('SELECT * FROM user WHERE username = :value AND password = :valuetwo');
+    $criteria = [
+        'value' => $uname,
+        'valuetwo' => $password
+    ];
+    $stmt->execute($criteria);
+    $resultAll = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+    if ($resultAll) {
+
+        $response = json_encode($resultAll);
+        $data = json_decode($response, true); // Decodes the JSON array into an associative array
+        $id = $data[0]['id'];
+        $name = $data[0]['name'];
+
+        // Used session to store user details
+        $_SESSION['user_id'] = $id;
+        $_SESSION['user_name'] = $name;
+
+        //if there is an matching row, then the page will redirected to home.php
+        header('location:courseSelectionForm.php');
+    } else {
+        // if there is no matching row then it will print " Something went Wrong"
+        echo '<script>alert("You have entered an incorrect username or password.");</script>';
+    }
+
+}
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="layout.css">
+    <title>University of Northampton Course Details</title>
+</head>
+
+<body>
+    <header>
+        <!-- html page header -->
+        <h3 style="text-align: center;">Login Page</h3>
+    </header>
+
+    <main>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+        <div class="container" style="display: flex; align-items: center; ">
+
+            <form method="POST" action="#">
+                <div class="form-input my-4">
+                    <label>Username: </label>
+                    <input type="username" name="username" required placeholder="Enter the Username" />
+                    <!-- input textbox to inserting the username -->
+                </div>
+                <div class="form-input my-3">
+                    <label>Password: </label>
+                    <input type="password" name="password" required placeholder="Enter the password" />
+                    <!-- input textbox to inserting the password -->
+                </div>
+                <div style="text-align: center;">
+                    <input type="submit" type="submit" value="LOG IN" class="btn-login" />
+                    <!-- Input element for submitting a login details -->
+                </div>
+            </form>
+        </div>
+    </main>
+    <footer>&copy; CSYM019 2023</footer>
+</body>
+
+</html>
