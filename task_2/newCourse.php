ce3e59df03d5a69ce1b31fbbd2587fcb8bf3d12e
shahadkariyambath <shahadchikku@gmail.com>
2023-05-28 22:17:54 +0100

    made some changes in newcourse.php

--- task_2/newCourse.php
+++ task_2/newCourse.php
@@ -90,8 +90,7 @@ if (isset($_POST['title'])) {
 <head>
     <title>Course Report</title>
     <link rel="stylesheet" href="layout.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
