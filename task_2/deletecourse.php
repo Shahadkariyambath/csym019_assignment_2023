fed2da802c1a03a95779e3d04e1a87902988e36b
shahadkariyambath <shahadchikku@gmail.com>
2023-05-31 23:20:43 +0100

    add code for deleting modeules from the database along with course detailes

--- task_2/deletecourse.php
+++ task_2/deletecourse.php
@@ -4,12 +4,15 @@ echo '<script>alert("' . $_GET['deleteid'] . '");</script>';
 if (isset($_GET['deletecourseid'])) {
     $id = $_GET['deletecourseid']; //takes the deleteid from the delete button and store it to $id variable
     $stmt = $pdo->prepare("delete from coursedetail where id=$id"); //delete statement
-
     if ($stmt->execute()) {
         //echo "Record deleted successfully";
-        echo '<script>alert("Course deleted successfully.");</script>';
+        $stmt2 = $pdo->prepare("delete from modulecredits where courseid=$id"); //delete statement
 
-        header('location:courseSelectionForm.php');
+        if ($stmt2->execute()) {
+
+            echo '<script>alert("Course deleted successfully.");</script>';
+            header('location:courseSelectionForm.php');
+        }
     } else {
         echo '<script>alert("Course is not deleted.");</script>';
 
